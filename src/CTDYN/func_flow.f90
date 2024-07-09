08cc4132a3705cc4c3cda9d7e33ccd85dca7c480
sbreton <breton.sylvain0971@gmail.com>
2024-07-09 11:45:22 +0200

    Refactoring

---
+++ src/CTDYN/func_flow.f90
@@ -0,0 +1,109 @@
+subroutine stream(x, ax, axp,  bx, bxp, fx)
+  """
+  Stream function and density / normalized
+  output 
+
+  STREAM FUNCTION DEFINITION AND NORMALIZATION  (one cell in latitude)
+
+  P2  = (3 cos(theta)**2 - 1) / 2
+
+  P12   = 3 cos(theta) sin(theta)  = - Diff (P2, theta) 
+ 
+  Define the stream function as:        Psi = fx * P12     
+
+  Up = - Curl (0,0,Psi) = (3 fx (1 - 3 cos^2) /x , 3 (fx x)'/x  sin cos, 0) =  (ax * (1- 3 cos^2) , bx sin cos, 0) = input code!
+  
+  so that Div (Up) = 0
+
+  But mass conservation implies  Div ( rho Up ) = 0
+ 
+  thus :    
+ 
+  Up  ->  Up/rho   
+ 
+  Ur =  -(3*cos(x)^2-1) fx / x   =>  ax = fx/x/rho       see eq.10a
+  
+  Utheta = (x fx)'/x  cos sin     =>  bx = (x fx )'  / x / rho   see eq.10b
+
+  so that Div ( rho Up ) = 0
+
+  Bullard-Gellard formalism:
+
+  Curl Curl (s2 P2, 0, 0) = (- 3 fx/x^2 * (3 cos^2 -1 ), -3 fx'/x * sin cos, 0)  
+  """
+  implicit none
+  use cio
+
+  real :: x_in, bct, c3, mmm,sr,rotp,gd, aqu, flg
+  common/ppar/x_in,bct,c3,mmm,sr,rotp,gd, aqu, flg
+  real :: xa1,xa2,xa3,xb,xda1,xda2
+  common/apar/xa1,xa2,xa3,xb,xda1,xda2
+  real :: s0,s2,s4,s6,a2p,a4p,xm
+  common/psi/s0,s2,s4,s6,a2p,a4p,xm
+  real :: ur, ut
+  real :: rho     
+  real :: xi, xo, xd, y1, y2
+  real :: norm, m1,m2,m3,m4
+  
+  character*52 :: dir
+  character*8 :: ans1,ans2,ans3,ans4
+  common/var3/ans1,ans2,ans3,ans4,dir
+ 
+  real :: x, me
+
+  if (x .ge. xb) then   
+    sigma=s2
+    x0=s0
+
+    fx=(1 - dexp(-(x - xb)**2/sigma**2))*(-1 + x)*x**2
+
+    ax=-(((-1 + dexp((x - xb)**2/sigma**2))*(-1 + x)*(1 - x0)**gd)/ &
+      &    (dexp(((-1 + x)*(1 + x - 2*xb))/sigma**2)*  &
+      &      (-1 + dexp((-1 + xb)**2/sigma**2))*(1/x - x0)**gd)) 
+
+    axp=((1 - x0)**gd*(-((-1 + dexp((x - xb)**2/sigma**2))*gd*sigma**2) + & 
+      &      x*((-1 + dexp((x - xb)**2/sigma**2))*(1 + gd)*sigma**2 + 2*xb - &
+      &         x*(2 + (-1 + dexp((x - xb)**2/sigma**2))*sigma**2*x0 + 2*x**2*x0 + & 
+      &            2*(1 + x0)*xb - 2*x*(1 + x0 + x0*xb)))))/ &
+      &  (dexp(((-1 + x)*(1 + x - 2*xb))/sigma**2)* &
+      &    (-1 + dexp((-1 + xb)**2/sigma**2))*sigma**2*x*(1/x - x0)**gd*(-1 + x*x0))
+
+
+    bx=-((dexp((1 - xb)**2/sigma**2 - (x - xb)**2/sigma**2)*(1 - x0)**gd*  &
+      &      (2*sigma**2 - 3*sigma**2*x - 2*x**2 + 2*x**3 + &
+      &        dexp((x - xb)**2/sigma**2)*sigma**2*(-2 + 3*x) + 2*x*xb - 2*x**2*xb)) &
+      &     /((-sigma**2 + dexp((1 - xb)**2/sigma**2)*sigma**2)*(1/x - x0)**gd))
+
+    bxp= ((1 - x0)**gd*(-2*(-1 + dexp((x - xb)**2/sigma**2))*gd*sigma**4 + &
+      &      4*x**6*x0 + sigma**2*x* &
+      &       (3*(-1 + dexp((x - xb)**2/sigma**2))*(1 + gd)*sigma**2 + &
+      &         2*(3 + gd)*xb) - 4*x**5*(1 + x0 + 2*x0*xb) + &
+      &      x**2*(-2*(4 + gd)*sigma**2 - &
+      &         3*(-1 + dexp((x - xb)**2/sigma**2))*sigma**4*x0 - &
+      &         2*sigma**2*(5 + gd + 3*x0)*xb + 4*xb**2) + &
+      &      2*x**3*(-2*xb*(2 + xb + x0*xb) + &
+      &         sigma**2*(6 + gd + 4*x0 + 5*x0*xb)) + & 
+      &      4*x**4*(1 - 3*sigma**2*x0 + xb*(2 + x0*(2 + xb)))))/ &
+      &  (dexp(((-1 + x)*(1 + x - 2*xb))/sigma**2)* &
+      &    (-1 + dexp((-1 + xb)**2/sigma**2))*sigma**4*x*(1/x - x0)**gd*(-1 + x*x0))
+
+    ! important you multiply by 2 because you then divide by sin_theta cos_theta 
+    fx=fx*2
+    ax=ax*2
+    axp=axp*2
+    bx = bx*2
+    bxp=bxp*2
+
+  else 
+    fx=0
+    ax=0
+    axp=0
+    bx=0
+    bxp=0
+  endif
+  return
+end
+
+use b2func, b3func, b4func, b5func
+use c1s2func, c3s2func, cdfunc
+
---
+++ src/CTDYN/hunt.f90
@@ -0,0 +1,91 @@
+subroutine hunt(xx,n,x,jlo)
+
+  integer jlo,n
+  real x,xx(n)
+  integer inc,jhi,jm
+  logical ascnd
+
+  ascnd=xx(n).gt.xx(1)
+  if (jlo .le. 0 .or. jlo .gt. n) then
+    jlo=0
+    jhi=n+1
+    goto 3
+  endif
+  inc=1
+  if (x.ge.xx(jlo).eqv.ascnd) then
+1   jhi=jlo+inc
+    if(jhi.gt.n)then
+      jhi=n+1
+    else if (x.ge.xx(jhi).eqv.ascnd) then
+      jlo=jhi
+      inc=inc+inc
+      goto 1
+    endif
+  else
+    jhi=jlo
+2   jlo=jhi-inc
+    if (jlo.lt.1) then
+      jlo=0
+    else if (x.lt.xx(jlo).eqv.ascnd) then
+      jhi=jlo
+      inc=inc+inc
+      goto 2
+    endif
+  endif
+3 if(jhi-jlo.eq.1) then 
+    return
+  endif 
+  jm=(jhi+jlo)/2
+  if (x.gt.xx(jm).eqv.ascnd) then
+    jlo=jm
+  else
+    jhi=jm
+  endif
+  goto 3
+end
+
+function factrl(n)
+  integer :: n
+  real :: factrl
+  integer :: j, ntop
+  real :: a(33),gammln
+  save ntop,a
+  data ntop,a(1)/0,1./
+
+  if (n.lt.0) then
+    write (*,*) 'Negative factorial in factrl'
+  else if (n.le.ntop) then
+    factrl=a(n+1)
+  else if (n.le.32) then
+    do j=ntop+1,n
+      a(j+1)=j*a(j)
+    enddo
+    ntop=n
+    factrl=a(n+1)
+  else
+    factrl=exp(gammln(n+1.))
+  endif
+  return
+end
+
+function gammln(xx)
+  real :: gammln, xx
+  integer :: j
+  double precision :: ser,stp,tmp,x,y,cof(6)
+  save cof,stp
+  data cof,stp/76.18009172947146d0,-86.50532032941677d0, &
+       & 24.01409824083091d0,-1.231739572450155d0,.1208650973866179d-2, &
+       & -.5395239384953d-5,2.5066282746310005d0/
+
+  x=xx
+  y=x
+  tmp=x+5.5d0
+  tmp=(x+0.5d0)*log(tmp)-tmp
+  ser=1.000000000190015d0
+  do j=1,6
+    y=y+1.d0
+    ser=ser+cof(j)/y
+  enddo
+  gammln=tmp+log(stp*ser/x)
+  return
+end
