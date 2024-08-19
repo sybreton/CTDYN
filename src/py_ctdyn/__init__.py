a25646797e7469a9ff3a9ab05b73a640df790037
sbreton <breton.sylvain0971@gmail.com>
2024-08-19 14:58:32 +0200

    Add function necessary to run CTDYN through a Python interface

--- src/py_ctdyn/__init__.py
+++ src/py_ctdyn/__init__.py
@@ -2,6 +2,12 @@ from importlib.metadata import version
 
 __version__ = version ('py_ctdyn')
 
+import py_ctdyn.templates 
+
+from .ctdyn import (get_ctdyn_dir,
+                    run_ctdyn,
+                    set_default_inlist)
+
 from .outputs import (read_field, 
                       read_field_text_file,
                       read_butterfly_diagram,
