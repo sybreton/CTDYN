a1f4a4b7620de17d428bdadfc65dd17bc4ae0c52
Sylvain <48833729+sybreton@users.noreply.github.com>
2024-09-04 20:51:00 +0200

    Create test and documentation (with versioning of main and dev branches) workflows (#1)
    * Add pipeline to deploy documentation
    
    * Add branch name for workflow
    
    * Add missing runs-on
    
    * Change doc path for sphinx-build call
    
    * Set correct path
    
    * Update sphinx workflow
    
    * Add ipython installation
    
    * Add test pipeline
    
    * Correct workflow
    
    * Update to deploy documentation on netlify
    
    * Add missing netlify installation
    
    * Update sphinx requirement settings
    
    * Add missing sphinx installation
    
    * Remove tests for windows
    
    * Update documentation pipeline
    
    * Correct indentation
    
    * Update required sphinx version
    
    * Fix installation step workflow
    
    * Fix and update workflow trigger
    
    * Fix syntax
    
    * Fix Exception message for environment variable and move corresponding test to execution tests
    
    * Add setup file
    
    * Remove module import from conf file
    
    * Specify branches for push
    
    * Update build path
    
    * Increment python required version to 3.9
    
    * Update sphinx recipe to use different python version
    
    * Provide an implementation for doc versioning
    
    * Trigger workflow
    
    * Fix checkout syntax
    
    * Fix echo syntax
    
    * Avoid cleaning before main checkout
    
    * Update workflow to display versioning
    
    * Create composite action for sphinx build
    
    * Modify string setting
    
    * Modify string setting
    
    * Update path to action
    
    * Add EOF mention
    
    * Update action setting
    
    * Fix repo name
    
    * Update workflow
    
    * Update workflow to export to PyData theme
    
    * Update script path
    
    * Fix syntax error
    
    * Add github_repo in arguments
    
    * Update documentation with cat for created json
    
    * Update create_json call
    
    * Update json creation procedure to fix issues
    
    * Fix syntax
    
    * Update procedure to create json file
    
    * Change step order
    
    * Update fetch depth before creating json file
    
    * Update procedure
    
    * Show found versions
    
    * Try making fetched branch visible
    
    * Hard code fetching of required branches
    
    * Update conf to use pydata theme
    
    * Update triggers
    
    * Specify new version value for action and use environment variable

---
+++ .github/workflows/create_json.py
@@ -0,0 +1,62 @@
+from git import Repo
+import sys, os, json
+import argparse
+
+def create_json (filename,
+                 url_template,
+                 include=[],
+                 latest_version="dev") :
+  """
+  Create json file.
+   
+  ``url_template`` should be such as 
+  ``https://mysite.org/``
+  """
+  repo = Repo( search_parent_directories=True )
+  versions = [branch.name for branch in repo.branches]
+  print ("Found following branches on repo: {}".format (versions))
+  json_list = []
+  
+  for version in versions:
+    if version in include :
+      if version==latest_version :
+        version = "latest"
+      version_dict = {
+                      "version":version,
+                      "url":os.path.join (url_template, version),
+                      }
+      json_list.append (version_dict)
+
+  with open(filename, 'w') as fp:
+    json.dump(json_list, fp)
+
+if __name__ == "__main__" :
+
+  CLI = argparse.ArgumentParser()
+  CLI.add_argument(
+    "--filename",  
+    nargs="?", 
+    type=str,
+    default="switcher.json", 
+    )
+  CLI.add_argument(
+    "--url",  
+    nargs="?", 
+    type=str,
+    default="url_not_specified", 
+    )
+  CLI.add_argument(
+    "--branches",  
+    nargs="*", 
+    type=str,
+    default=["main", "dev"], 
+    )
+  CLI.add_argument(
+    "--latest",  
+    nargs="?", 
+    type=str,
+    default="dev", 
+    )
+  args = CLI.parse_args()
+  print (vars(args).values())
+  create_json (*vars(args).values())
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,6 @@ autodoc_mock_imports = ['numpy', 'scipy', 'matplotlib',
 project = 'CTDYN'
 copyright = '2024, A. Bonanno, S.N. Breton'
 author = 'A. Bonanno, S.N. Breton, G. Licciardello'
-release = '0.1'
 master_doc = 'index'
 source_suffix = ".rst"
 
@@ -37,7 +36,6 @@ version = __version__
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.napoleon',
               'sphinx.ext.mathjax',
-              'sphinx_book_theme',
               'IPython.sphinxext.ipython_console_highlighting']
 
 napoleon_numpy_docstring = True
@@ -45,26 +43,33 @@ napoleon_numpy_docstring = True
 exclude_patterns = []
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_book_theme'
-html_title = "star-privateer"
+html_theme = "pydata_sphinx_theme"
+html_title = "CTDYN"
 html_logo = "logo.png"
-html_theme_options = {
-     "path_to_docs": "docs",
-     "repository_url": "https://gitlab.com/sybreton/star_privateer/",
-     "use_repository_button": True,
-     "use_download_button": True,
-     }
-html_sidebars = {
-    "**": [
-     "navbar-logo.html",
-     "search-field.html",
-     "sbt-sidebar-nav.html",
-          ]
-     }
-numpydoc_show_class_members = False
 
+json_url = "https://ctdyn.netlify.app/latest/_static/switcher.json"
+
+html_context = {"github_repo":"https://github.com/sybreton/CTDYN"}
+html_theme_options = dict ()
+html_theme_options["navbar_start"] = ["navbar-logo", 
+                                      "version-switcher"]
+html_theme_options["navbar_end"] = ["navbar-icon-links"] 
+
+current_version = __version__
+# Dev branch set to be the latest 
+if current_version=="dev" :
+  current_version = "latest"
+html_theme_options["switcher"] = {"json_url":json_url, 
+                                  "version_match":current_version}
+
+html_theme_options["check_switcher"] = False
+
+html_sidebars = {
+    "**": ["sidebar-nav-bs", "sidebar-ethical-ads"]
+}
+
+numpydoc_show_class_members = False
 html_static_path = []
